f73930ed94ecdf054e3825635715fd610f9689fa
ronen <ronensroussi@gmail.com>
2019-05-30 18:45:33 +0300

    bla bla

--- src/State.java
+++ src/State.java
@@ -1,3 +1,21 @@
 public interface State {
+
+    public void turnOn();
+    public void turnOff();
+    public void internetOn();
+    public void internetOff();
+    public void fileRequest();
+
+    public void downloadAborted();
+    public void downloadError();
+    public void errorFixed();
+    public void downloadFineshed();
+
+    public void movieOn();
+    public void restartMovie();
+    public void holdMovie();
+    public void movieOff();
+    public void resume();
     
+
 }
