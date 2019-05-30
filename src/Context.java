94c1e432bc87003a7f2ed996cdb65440343c9200
J-morag <38430800+J-morag@users.noreply.github.com>
2019-05-30 18:28:15 +0300

    added skeleton for project packages

--- src/Context.java
+++ src/Context.java
@@ -1,38 +1,38 @@
 public class Context {
 
-    private State region1State;
-    private State region2State;
-    private State region3State;
-    private State region4State;
+    private State region1Downloads;
+    private State region2Connection;
+    private State region3Player;
+    private State region4User;
 
     // region1 states
-    private final State waitingForDownloads;
-    private final State fixingError;
-    private final State downloadingFile;
-    private final State checkingDiskSpace;
-    private final State waitingForInternetConnection;
+    private final State downloads_waitingForDownloads;
+    private final State downloads_fixingError;
+    private final State downloads_downloadingFile;
+    private final State downloads_checkingDiskSpace;
+    private final State downloads_waitingForInternetConnection;
 
     // region2 states
-    private final State noConnection;
-    private final State hasConnection;
+    private final State connection_noConnection;
+    private final State connection_hasConnection;
 
     // region3 states
-    private final State idle;
-    private final State downloadingMovie;
-    private final State moviePaused;
-    private final State moviePlaying;
+    private final State player_idle;
+    private final State player_downloadingMovie;
+    private final State player_moviePaused;
+    private final State player_moviePlaying;
 
     // region4 states
-    private final State checkStatus;
-    private final State beginner;
-    private final State advanced;
-    private final State professional;
+    private final State user_checkStatus;
+    private final State user_beginner;
+    private final State user_advanced;
+    private final State user_professional;
 
     public Context() {
-        this.region1State = waitingForDownloads;
-        this.region2State = noConnection;
-        this.region3State = idle;
-        this.region4State = checkStatus;
+        this.region1Downloads = downloads_waitingForDownloads;
+        this.region2Connection = connection_noConnection;
+        this.region3Player = player_idle;
+        this.region4User = user_checkStatus;
     }
 
 
