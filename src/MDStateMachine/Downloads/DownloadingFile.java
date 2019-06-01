d64ff424087ee6ba928906bed34c5f857617cee4
ronen <ronensroussi@gmail.com>
2019-06-01 10:03:12 +0300

    bla bla

--- src/MDStateMachine/Downloads/DownloadingFile.java
+++ src/MDStateMachine/Downloads/DownloadingFile.java
@@ -13,18 +13,27 @@ public class DownloadingFile extends AMDSate {
     @Override
     public void downloadFinished() {
         super.downloadFinished();
+        context.userPoints++;
+        context.setDownloadsRegion_currentState(context.downloads_waitingForDownloads);
+
     }
 
     @Override
     public void downloadAborted() {
         super.downloadAborted();
         context.deleteRequestFromQueue();
+        if(context.userPoints>0) {
+            context.userPoints--;
+        }
         context.downloadFailed();
+        context.setDownloadsRegion_currentState(context.downloads_waitingForDownloads);
     }
 
     @Override
     public void startDownloading() {
+
         super.startDownloading();
+        context.deleteRequestFromQueue();
     }
 
     @Override
